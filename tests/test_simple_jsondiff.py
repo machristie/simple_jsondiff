a21fb6d163d9c4486bd1b7e1cf7608264e9d22ab
Marcus Christie <machrist@iu.edu>
2018-10-14 11:27:16 -0700

    Test configuration

--- tests/test_simple_jsondiff.py
+++ tests/test_simple_jsondiff.py
@@ -3,6 +3,8 @@
 
 """Tests for `simple_jsondiff` package."""
 
+import json
+
 import pytest
 
 from click.testing import CliRunner
@@ -36,3 +38,23 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_jsondiff():
+    diff = simple_jsondiff.jsondiff("""
+    {
+        "a": 1,
+        "b": 3
+    }
+    """, """
+    {
+        "a": 2,
+        "b": 3,
+        "c": 4
+    }
+    """)
+    diff_dict = json.loads(diff)
+    assert "a" in diff_dict, "value of 'a' changed so should be in diff"
+    assert "b" not in diff_dict, "value of 'b' didn't change"
+    assert "c" in diff_dict, "'c' was added"
+    assert diff_dict["a"] == 2, "value of 'a' changed to 2"
